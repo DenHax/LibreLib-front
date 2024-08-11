aefbb04ef36ddf3b3fca66bcf556f1c8de88abf2
DenHax <whatxpw@gmail.com>
2024-08-11 15:22:36 +0300

    Initial for LibreLib frontend in ReactTS

---
+++ devenv.nix
@@ -0,0 +1,59 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+let
+  pkgs-stable = import inputs.nixpkgs-stable { system = pkgs.stdenv.system; };
+in
+{
+  env.GREET = "devenv";
+
+  packages = [ pkgs.git ];
+
+  languages.javascript = {
+    enable = true;
+    package = pkgs-stable.nodejs_22;
+    npm.enable = true;
+    pnpm = {
+      enable = true;
+    };
+    yarn.enable = false;
+  };
+
+  pre-commit.hooks = {
+    prettier = {
+      enable = true;
+      package = pkgs.prettierd;
+      settings = {
+        binPath = "${pkgs.prettierd}/bin/prettierd";
+      };
+    };
+
+    eslint = {
+      enable = true;
+      package = pkgs.nodePackages.eslint;
+    };
+  };
+
+  scripts.hello.exec = # bash
+    ''
+      echo hello from $GREET
+    '';
+
+  enterShell = # bash
+    ''
+      echo "Frontend for LibreLib"
+      node --version
+      pnpm --version
+    '';
+
+  enterTest = # bash
+    ''
+      echo "Running tests"
+      git --version | grep --color=auto "${pkgs.git.version}"
+    '';
+
+}
---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+  const [inputValue, setInput] = useState("");
+
+  const handleName = () => {
+    setName(document.querySelector("#name-input").value);
+  };
+
+  return (
+    <>
+      <h2>Enter You name:</h2>
+      <div className="card">
+        <input
+          type="text"
+          name="input"
+          value={inputValue}
+          id="name-input"
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <button onClick={() => handleName()}>Enter</button>
+        <br />
+        <button onClick={() => setCount((count) => count + 1)}>{count}</button>
+        <p>
+          Greet you {count} times, {name}!
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
